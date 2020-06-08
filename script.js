5ee85e59dbc23ca7f34bfe49a064a6dc8d718fe3
Amanda Pearce <amandagpearce@gmail.com>
2020-06-08 05:42:12 -0300

    Refactoring of requestOutput function and ES6 syntax

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-	
-     var button = document.getElementById('load');
-     button.addEventListener('click', myLoader, false)
+
+     let button = document.getElementById('load');
+     //button.addEventListener('click', myLoader, false)
      
-     var capital="";
-     var output = document.getElementById('output');
+     let capital="";
+     let output = document.getElementById('output');
+     let countriesObj;
 
-     var flag = 3;
-     var gameplay = false;
-     var rightGuesses=0;
+     let flag = 3;
+     let gameplay = false;
+     let rightGuesses=0;
 
-     function init(options,country){
+     let hearts = document.querySelectorAll('#heart-block > div span');
+
+     let init = (options,country) => {
  		document.querySelector('#question span').innerHTML=country;
-     	var html="";
-     	for(var i=0; i<options.length; i++){
+     	let html="";
+     	for(let i=0; i<options.length; i++){
      		html += '<li><a class="opt" onclick="checkAnswer()" href="javascript:;" data-value="'+options[i]+'">'+options[i]+'</a></li>';
      	}
      	document.getElementById('options').innerHTML = html;
      	
      }
 
-     function shuffleArray(arr) {
-        for (var x = arr.length - 1; x > 0; x--) {
-            var holder = Math.floor(Math.random() * (x + 1));
-            var temp = arr[x];
+     let shuffleArray = (arr) => {
+        for(let x = arr.length - 1; x > 0; x--) {
+            let holder = Math.floor(Math.random() * (x + 1));
+            let temp = arr[x];
             arr[x] = arr[holder];
             arr[holder] = temp;
         }
         return arr;
     }
 
-     function myLoader(){
+     let myLoader = () => {
      	button.classList.add('hide');
 
-     	var xHR = new XMLHttpRequest();
+     	const xHR = new XMLHttpRequest();
      	xHR.open('get','https://restcountries.eu/rest/v2/all',true);
      	xHR.send();
 
@@ -40,55 +43,61 @@
      }
 
      function requestOutput(){
-     	document.getElementById('question').classList.remove('hide');
-     	document.getElementById('heart-block').classList.remove('hide');
+        countriesObj = JSON.parse(this.responseText);
+        //console.log(countriesObj);
+
+     	showLives();
      	gameplay=true;
 
      	output.innerHTML=" ";
-     	countriesObj = JSON.parse(this.responseText);
-     	//console.log(countriesObj);
-
-     	var randomPick;
-     	var randomOpts="";
-     	var randomCapitals="";
-     	var opts=[];
-     	var country = "";
-
-     	randomPick = Math.floor(Math.random()*countriesObj.length);
-     	country = countriesObj[randomPick].name;
-     	capital = countriesObj[randomPick].capital
-     	if(!capital.length || capital.length===0 || capital==null || capital==""){
-     			capital="It does not have one";
-     			opts.push(capital);
-     		}
-     	else{
-     		opts.push(capital);
-     	}
-     	//console.log(country+'-'+capital);
-
-     	for(var x=0; x<3;x++){
-     		randomOpts = Math.floor(Math.random()*countriesObj.length);
-     		randomCapitals = countriesObj[randomOpts].capital;
-     		if(!randomCapitals.length || randomCapitals.length===0 || randomCapitals==null || randomCapitals==""){
-     			randomCapitals="It does not have one";
-     			opts.push(randomCapitals);
-     		}
-     		else{
-     			opts.push(randomCapitals);
-     		}
-     	}
-     	//console.log(opts);
-     	shuffleArray(opts);
-     	init(opts,country);
+     	
+     	pickRandomCountry();
      }
 
-     function checkAnswer(){
+    let showLives = () => {
+        document.getElementById('question').classList.remove('hide');
+        document.getElementById('heart-block').classList.remove('hide');
+     }
+
+     let pickRandomCountry = () => {
+        let randomPick;
+        let randomOpts="";
+        let randomCapitals="";
+        let opts=[];
+        let country = "";
+
+        randomPick = Math.floor(Math.random()*countriesObj.length);
+        country = countriesObj[randomPick].name;
+        capital = countriesObj[randomPick].capital
+        if(!capital.length || capital.length===0 || capital==null || capital==""){
+                capital="It does not have one";
+                opts.push(capital);
+            }
+        else{
+            opts.push(capital);
+        }
+        //console.log(country+'-'+capital);
+
+        for(let x=0; x<3;x++){
+            randomOpts = Math.floor(Math.random()*countriesObj.length);
+            randomCapitals = countriesObj[randomOpts].capital;
+            if(!randomCapitals.length || randomCapitals.length===0 || randomCapitals==null || randomCapitals==""){
+                randomCapitals="It does not have one";
+                opts.push(randomCapitals);
+            }
+            else{
+                opts.push(randomCapitals);
+            }
+        }
+        //console.log(opts);
+        shuffleArray(opts);
+        init(opts,country);
+     }
+
+     let checkAnswer = () => {
      	if(gameplay){
      		event.target.classList.add('active');
-	     	var guess = event.target.dataset.value;
-	     	var output = document.getElementById('output');
-	     	var hearts = document.querySelectorAll('#heart-block > div span');
-	     	
+	     	let guess = event.target.dataset.value;
 	     	
 	     	if(guess==capital){
 	     		output.innerHTML = "<h4>You're right!</h4>";
@@ -106,18 +115,8 @@
 	     			else{
 	     				output.innerHTML += "<h3>You got "+rightGuesses+" capitals right</h3>"
 	     			}
-	     			
 	     			output.innerHTML += "<br><h4 class='blink'>Game over</h4>";
-	     			button.value='Restart';
-	     			button.classList.remove('hide');
-
-	     			for(var x=0; x<hearts.length; x++){
-	     				hearts[x].classList.remove('grey');
-	     			}
-	     			document.getElementById('heart-block').classList.add('hide');
-	     			flag=3;
-	     			rightGuesses=0;
-	     			gameplay=false;
+                    resetGame();
 	     		}
 	     		else {
 	     			window.setTimeout(myLoader, 2000);
@@ -127,6 +126,18 @@
 
      }
 
+    let resetGame = () =>{
+        button.value='Restart';
+        button.classList.remove('hide');
+
+        for(let x=0; x<hearts.length; x++){
+            hearts[x].classList.remove('grey');
+        }
+        document.getElementById('heart-block').classList.add('hide');
+        flag=3;
+        rightGuesses=0;
+        gameplay=false;
+    }
 
 
 	
